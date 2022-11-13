f74bd7d8d2088c0f9c686e4e482f11cd69e8b5ec
Artem <artemkostrikin12@gmail.com>
2022-11-13 18:09:19 +0300

    cod

--- Program.cs
+++ Program.cs
@@ -3,7 +3,12 @@ string[] array2 = new string[array1.Length];
 int count = 0;
 void SecondArrayWithIF(string[] array1, string[] array2)
 {
-    int count = 0;
     for (int i = 0; i < array1.Length; i++)
     {
     if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
